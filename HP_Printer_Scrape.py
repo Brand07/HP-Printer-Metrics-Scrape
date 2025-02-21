946c5a78d8abdf37ae1297b99aadb8da9fa6ba80
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-02-21 09:11:47 -0600

    Consolidate toner and maintenance kit level check since they're on the same page

--- HP_Printer_Scrape.py
+++ HP_Printer_Scrape.py
@@ -3,73 +3,73 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-
-"""
-Printer dictionary (NAME:PRINTER IP)
-Change or add the printer names/IP addresses for your environment.
-"""
+# Printer details
 printers = {
     "STN2": "10.10.113.150",
     "LTL1": "10.10.113.212",
     "CLB1": "10.10.113.180",
     "SPS2": "10.10.113.215",
     "STN1": "10.10.113.213",
-    "SPS1": "10.10.113.214"
+    # "SPS1": "10.10.113.214"
 }
 
-# Tags we're looking for
-page_count_tag = 'UsagePage.ImpressionsByMediaSizeTable.Print.Letter.Total'
-toner_tag = 'SupplyGauge0'
-maintenance_tag = 'SupplyGauge1'
+# Tags for scraping
+page_count_tag = "UsagePage.ImpressionsByMediaSizeTable.Print.Letter.Total"
+toner_tag = "SupplyGauge0"
+maintenance_tag = "SupplyGauge1"
 
-def get_data(url, tag, element='td'):
+
+def get_data(url, tags, element="td"):
     try:
+        print(f"Getting data for {url}")
         response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        data = soup.find(element, {'id': tag}).text.replace(',', '')
+        soup = BeautifulSoup(response.text, "html.parser")
+        data = {
+            tag: soup.find(element, {"id": tag}).text.replace(",", "") for tag in tags
+        }
         return data
     except requests.exceptions.RequestException:
-        print(f"Issue getting data from {url}...")
-        return None
+        print(f"Issue gathering data from {url}...")
+        return {tag: None for tag in tags}
 
-# Collect and store the data in an empty list
-data =[]
-for name, ip, in printers.items():
-    page_count_url = f'http://{ip}/hp/device/InternalPages/Index?id=UsagePage'
-    device_status_url = f'http://{ip}/hp/device/DeviceStatus/Index'
 
-    page_count = get_data(page_count_url, page_count_tag)
-    toner_level = get_data(device_status_url, toner_tag, 'span')
-    maint_level = get_data(device_status_url, maintenance_tag, 'span')
+# Collect data for each printer
+data = []
+for name, ip in printers.items():
+    page_count_url = f"http://{ip}/hp/device/InternalPages/Index?id=UsagePage"
+    device_status_url = f"http://{ip}/hp/device/DeviceStatus/Index"
 
-    #Append the data to the empty list
-    data.append({
-        'Printer': name,
-        'Page Count': page_count,
-        'Toner Level': toner_level,
-        'Maintenance Level': maint_level
-    })
+    page_count = get_data(page_count_url, [page_count_tag])[page_count_tag]
+    device_status = get_data(device_status_url, [toner_tag, maintenance_tag], "span")
+
+    data.append(
+        {
+            "Printer": name,
+            "Page Count": page_count,
+            "Toner Level": device_status[toner_tag],
+            "Maintenance Level": device_status[maintenance_tag],
+        }
+    )
 
 # Get the current date
 now = datetime.now()
 date_string = now.strftime("%m-%d-%Y")
 
-# Create the pandas df for the excel expor
+# Create the Pandas DataFrame for the Excel export
 df = pd.DataFrame(data)
-df['Date'] = date_string
+df["Date"] = date_string
 
-# Try to load the previous file to append new data
+# Attempt to load the previous data from Excel if it exists
 try:
-    prev_df = pd.read_excel('Printer_Metrics.xlsx')
+    print("Looking for existing .xlsx file")
+    prev_df = pd.read_excel("Printer_Metrics.xlsx")
 except FileNotFoundError:
     prev_df = pd.DataFrame()
 
-# Append the new data to the existing file
+# Append new data to the previous data
+print("Appending the data")
 df = pd.concat([prev_df, df], ignore_index=True)
 
-# Write the data to the excel file
-df.to_excel('Printer_Metrics.xlsx', index=False)
-
-
-
-
+# Write to Excel
+df.to_excel("Printer_Metrics.xlsx", index=False)
+print("Data appended!")
