a179d5686dbc1ef2d9c77f5a03866b76931b8507
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2025-02-21 09:02:16 -0600

    rewrite to be more efficient

--- HP_Printer_Scrape.py
+++ HP_Printer_Scrape.py
@@ -4,117 +4,72 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 
-#Printers
-printer_1 = "STN2"
-printer_2 = "LTL1"
-printer_3 = "CLB1"
-printer_4 = "SPS2"
-printer_5 = "STN1"
-printer_6 = "SPS1"
-    
-#URLs for the Usage Metrics
-stn2_pcount_url = 'http://10.10.113.150/hp/device/InternalPages/Index?id=UsagePage'
-ltl1_pcount_url = 'http://10.10.113.212/hp/device/InternalPages/Index?id=UsagePage'
-clb1_pcount_url = 'http://10.10.113.180/hp/device/InternalPages/Index?id=UsagePage'
-sps2_pcount_url = 'http://10.10.113.215/hp/device/InternalPages/Index?id=UsagePage'
-stn1_pcount_url = 'http://10.10.113.213/hp/device/InternalPages/Index?id=UsagePage'
-sps1_pcount_url = 'http://10.10.113.214/hp/device/InternalPages/Index?id=UsagePage'
-#URLs for the consumable levels
-stn2_device_status = 'http://10.10.113.150/hp/device/DeviceStatus/Index'
-ltl1_device_status = 'http://10.10.113.212/hp/device/DeviceStatus/Index'
-clb1_device_status = 'http://10.10.113.180/hp/device/DeviceStatus/Index'
-sps2_device_status = 'http://10.10.113.215/hp/device/DeviceStatus/Index'
-stn1_device_status = 'http://10.10.113.213/hp/device/DeviceStatus/Index'
-sps1_device_status = 'http://10.10.113.214/hp/device/DeviceStatus/Index'
+"""
+Printer dictionary (NAME:PRINTER IP)
+Change or add the printer names/IP addresses for your environment.
+"""
+printers = {
+    "STN2": "10.10.113.150",
+    "LTL1": "10.10.113.212",
+    "CLB1": "10.10.113.180",
+    "SPS2": "10.10.113.215",
+    "STN1": "10.10.113.213",
+    "SPS1": "10.10.113.214"
+}
 
+# Tags we're looking for
 page_count_tag = 'UsagePage.ImpressionsByMediaSizeTable.Print.Letter.Total'
 toner_tag = 'SupplyGauge0'
 maintenance_tag = 'SupplyGauge1'
-"""
-This function gets allows you to pass the url of the page that 
-contains the printer's page count
-"""
-def get_page_count(url):
+
+def get_data(url, tag, element='td'):
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        data = soup.find('td', {'id': page_count_tag}).text
+        data = soup.find(element, {'id': tag}).text.replace(',', '')
         return data
     except requests.exceptions.RequestException:
-        print("Issue gathering Page Count...")
+        print(f"Issue getting data from {url}...")
         return None
 
+# Collect and store the data in an empty list
+data =[]
+for name, ip, in printers.items():
+    page_count_url = f'http://{ip}/hp/device/InternalPages/Index?id=UsagePage'
+    device_status_url = f'http://{ip}/hp/device/DeviceStatus/Index'
 
-def get_toner_level(url):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        data = soup.find('span', {'id': toner_tag}).text.replace(',','')
-        return data
-    except requests.exceptions.RequestException:
-        print("Issue gathering Toner Level...")
-        return None
+    page_count = get_data(page_count_url, page_count_tag)
+    toner_level = get_data(device_status_url, toner_tag, 'span')
+    maint_level = get_data(device_status_url, maintenance_tag, 'span')
 
-def get_maint_level(url):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        data = soup.find('span', {'id': maintenance_tag}).text.replace(',','')
-        return data
-    except requests.exceptions.RequestException:
-        print("Issue gathering Maintenance Level...")
-        return None
+    #Append the data to the empty list
+    data.append({
+        'Printer': name,
+        'Page Count': page_count,
+        'Toner Level': toner_level,
+        'Maintenance Level': maint_level
+    })
 
-
-#Page Counts for the printers   
-stn2_page_count = get_page_count(stn2_pcount_url)
-ltl1_page_count = get_page_count(ltl1_pcount_url)
-clb1_page_count = get_page_count(clb1_pcount_url)
-sps2_page_count = get_page_count(sps2_pcount_url)
-stn1_page_count = get_page_count(stn1_pcount_url)
-sps1_page_count = get_page_count(sps1_pcount_url)
-#Toner levlels for each printer.
-stn2_toner_level = get_toner_level(stn2_device_status)
-ltl1_toner_level = get_toner_level(ltl1_device_status)
-clb1_toner_level = get_toner_level(clb1_device_status)
-sps2_toner_level = get_toner_level(sps2_device_status)
-stn1_toner_level = get_toner_level(stn1_device_status)
-sps1_toner_level = get_toner_level(sps1_device_status)
-#Maintenance Kit Levels for Each Printer
-stn2_maint_level = get_maint_level(stn2_device_status)
-ltl1_maint_level = get_maint_level(ltl1_device_status)
-clb1_maint_level = get_maint_level(clb1_device_status)
-sps2_maint_level = get_maint_level(sps2_device_status)
-stn1_maint_level = get_maint_level(stn1_device_status)
-sps1_maint_level = get_maint_level(sps1_device_status)
-
-#Get the Curent Date
+# Get the current date
 now = datetime.now()
 date_string = now.strftime("%m-%d-%Y")
 
-#Create the Pandas Dataframe for the Excel export.
-df = pd.DataFrame({
-    'Date' : [date_string]*6,
-    'Printer': ['STN2', 'LTL1', 'CLB1', 'SPS2', 'STN1', 'SPS1'],
-    'Page Count': [stn2_page_count, ltl1_page_count, clb1_page_count, sps2_page_count, stn1_page_count, sps1_page_count],
-    'Toner Level': [stn2_toner_level, ltl1_toner_level, clb1_toner_level, sps2_toner_level, stn1_toner_level, sps1_toner_level],
-    'Maintenance Level': [stn2_maint_level, ltl1_maint_level, clb1_maint_level, sps2_maint_level, stn1_maint_level, sps1_maint_level]
-    
-})
+# Create the pandas df for the excel expor
+df = pd.DataFrame(data)
+df['Date'] = date_string
 
-"""
-Attemps to load the previous data from Excel if it exists.
-If not, it will create a new file in the running directory.
-"""
+# Try to load the previous file to append new data
 try:
-    prev_df =pd.read_excel('Printer_Metrics.xlsx')
+    prev_df = pd.read_excel('Printer_Metrics.xlsx')
 except FileNotFoundError:
-    prev_df =pd.DataFrame()
+    prev_df = pd.DataFrame()
 
-#Append new data to the previous data
+# Append the new data to the existing file
 df = pd.concat([prev_df, df], ignore_index=True)
 
-#Write to Excel 
+# Write the data to the excel file
 df.to_excel('Printer_Metrics.xlsx', index=False)
+
+
 
 
